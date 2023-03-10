7cc5faaa310e9e54bf59e26334a04dae240e261e
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-10 15:59:30 +0530

    feat: add contentData routes

---
+++ src/routes/api/content/contentData.route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const contentDataControllers = require('../../../controllers/content/contentData.controller');
+
+
+router.get('/', contentDataControllers.getAllDatasControllerById);
+
+router.post('/', contentDataControllers.createContentDataControllerById);
+
+router.patch('/:dataId', contentDataControllers.updateContentDataControllerById);
+
+router.delete('/:dataId', contentDataControllers.deleteContentDataControllerById);
+
+module.exports = router;
