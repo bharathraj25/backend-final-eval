69a3a8b1703b3bb0256d7ec3e31ebb897798b6ea
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-10 15:59:12 +0530

    feat: add content services

---
+++ src/services/content/content.service.js
@@ -0,0 +1,60 @@
+const db = require('../../../db/models');
+const errors = require('../../errors');
+
+const getAllContents = async (email) => {
+  const data = await db.Content.findAll({
+    'where': {
+      user_email: email
+    },
+    order: [
+      ['id', 'ASC'],
+      ['types', 'id', 'ASC'],
+      ['datas', 'id', 'ASC'],
+    ],
+    include: [
+      { model: db.ContentType, as: 'types' },
+      { model: db.ContentData, as: 'datas' }
+    ]
+  });
+  return data;
+};
+
+const createContent = async (contentName, email) => {
+  const newContent = await db.Content.create({
+    content_name: contentName,
+    user_email: email
+  });
+  return newContent;
+};
+
+const updateContentNameById = async (contentId, contentName, email) => {
+  const newContent = await db.Content.findByPk(contentId);
+  if (newContent.user_email !== email) {
+    throw new errors.HttpError(403, `content with contentId: ${contentId} - Not Accessable.`);
+  }
+  newContent['content_name'] = contentName;
+  newContent.save();
+  return newContent;
+};
+
+const deleteContentById = async (contentId, email) => {
+  // delete all instances of contentData and contentType
+  // added onDelete: 'cascade' to models/content.js
+
+  const status = await db.Content.destroy({
+    'where': {
+      'id': contentId,
+      'user_email': email
+    }
+  });
+
+  if (status === 0)
+    throw new errors.NotFoundError(`content with ${contentId} not found.`);
+};
+
+module.exports = {
+  getAllContents,
+  createContent,
+  updateContentNameById,
+  deleteContentById
+};
