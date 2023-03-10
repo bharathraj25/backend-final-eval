3608c8cccca176d864d5ce57f6867d0e44143def
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-10 15:59:44 +0530

    feat: add contentType routes

---
+++ src/routes/api/content/contentType.route.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const contentTypeControllers = require('../../../controllers/content/contentType.controller');
+
+router.get('/', contentTypeControllers.getAllTypesControllerById);
+
+router.post('/', contentTypeControllers.createContentTypeControllerById);
+
+router.patch('/:typeId', contentTypeControllers.updateContentTypeControllerById);
+
+router.delete('/:typeId', contentTypeControllers.deleteContentTypeControllerById);
+
+module.exports = router;
