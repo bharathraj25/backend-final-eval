13bb23715da7a9929419ef79eff48c57ebcb6a2e
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-10 15:59:01 +0530

    feat: add contentData services

---
+++ src/services/content/contentData.service.js
@@ -0,0 +1,60 @@
+const db = require('../../../db/models');
+const errors = require('../../errors');
+
+const getAllContentDatas = async (contentId) => {
+  const data = await db.ContentData.findAll({
+    'where': {
+      'content_id': contentId
+    },
+    order: [
+      ['id', 'ASC']
+    ],
+    include: [
+      { model: db.Content, as: 'content' }
+    ]
+  });
+  return data;
+};
+
+const createContentData = async (data, contentId, typeId) => {
+  const newContentData = await db.ContentData.create({
+    data: data,
+    content_id: contentId,
+    type_id: typeId,
+    include: [
+      { model: db.Content, as: 'content' }
+    ]
+  });
+  return newContentData;
+};
+
+const updateContentDataById = async (contentId, dataId, data) => {
+  const newContentData = await db.ContentData.findByPk(dataId);
+  newContentData['data'] = {
+    ...newContentData['data'],
+    ...data
+  };
+  newContentData.save();
+  return newContentData;
+};
+
+const deleteContentDataById = async (contentId, dataId) => {
+  const status = await db.ContentData.destroy({
+    'where': {
+      id: dataId,
+      content_id: contentId
+    }
+  });
+
+  // delete asc as well here
+  if (status === 0)
+    throw new errors.NotFoundError(`content with ${contentId} has no data with ${dataId} - NOT FOUND.`);
+
+};
+
+module.exports = {
+  getAllContentDatas,
+  createContentData,
+  updateContentDataById,
+  deleteContentDataById
+};
